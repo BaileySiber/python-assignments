ad5b7d19406039491680546e69a50adeb9e9d73e
baileysiber <bsiber@hks.harvard.edu>
2023-01-13 13:53:25 -0500

    task 5 also complete

--- filtering.py
+++ filtering.py
@@ -1,6 +1,25 @@
 import csv
+import json
 
 with open('output/veggies2.csv', 'r') as f:
     vegetables = csv.DictReader(f)
     rows = list(vegetables)
     print(rows)
+
+green_veggies = []
+
+with open('output/green_veggies.csv', 'w') as o:
+    writer = csv.writer(o)
+    for row in rows:
+        if row['color'] == 'green':
+            print(row)
+            writer.writerow([row['name'], row['color'], str(row['length'])])
+
+with open('output/green_veggies.json', 'w') as f:
+    green_veggies = []
+
+    for row in rows:
+        if row['color'] == 'green':
+            green_veggies.append(row)
+
+    json.dump(green_veggies, f, indent=2)
